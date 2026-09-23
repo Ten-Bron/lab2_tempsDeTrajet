b1781717489801deaf40019d1d7bbd59db05b297
lordelbardooo <tenakor.bron@heig-vd.ch>
2026-09-23 13:46:23 +0200

    version 1. exercice de base sans entree de parametres. Pseudo code + code

--- main.cpp
+++ main.cpp
@@ -1,7 +1,36 @@
-/* --------------------------- 
+#include <complex>
+#include <numbers>
+#include <iostream>
+#include <cmath>
+#include <stdlib.h>
+using namespace std;
+/* ---------------------------
 Laboratoire : 02
 Auteur(s) : 
 Date : 
 But : Calcul du temps de trajet 
 Remarque(s) : 
 --------------------------- */
+int main() {
+    const double s1 = 5; //km/h
+    const double s2 = 2; //km/h
+    const double dx = 3; //km
+    const double dy = 10; //km
+    const double L1 = 6; //km
+    double L2 = 0; //km
+
+    // calculating the second triangle's edge
+    const double d = dy - L1;
+    // calcul de L2: pythagoras, L2 being the hypo
+    L2 = sqrt(pow(dx,2)+ pow(d,2));
+    // calculating the time along L1
+    const double t1 = L1/s1;
+    // ccalculating the time along L2
+    const double t2 = L2/s2;
+    // sum of every delays
+    const double temps_total = t1 + t2;
+
+    cout << "Temps de trajet total:" << temps_total << endl;
+
+    return EXIT_SUCCESS;
+}
--- pseudo.md
+++ pseudo.md
@@ -1,1 +1,24 @@
 # Pseudo code de votre programme
+
+////////////////////////////
+PSEUDO CODE EXERCICE BASE:
+
+couche 1:
+1) de calcul de temps
+2) affiche le temps total.
+
+couche 2:
+1) calcul de la distance L2.
+2) calcul du temps de parcours sur la distance L1.
+3) calcul du temps de parcours sur la distance L2.
+4) somme des temps de parcours.
+5) affiche le temps total de parcours.
+6) 
+couche 3:
+1) calcul de la deuxieme cote: d = L1-dy =4, L2 etant l'hypotenuse du triangle aux arretes L2,dx et d
+2) calcul de L2 = sqrt((dy-L1)^2 + dx^2). formule de pythagoras.
+3) calcul du temps de parcours sur L1: t1 = L1/s1
+4) calcul du temps de parcours sur L2: t2 = L2/s2
+5) calcul de la somme des temps de trajet: L1 + L2
+6) afficher dans le terminal "Temps de trajet total:" suivi du temps de trajet total.
+
